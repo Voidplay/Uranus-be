f71beae6986f89af5ff7490167736c0d8149981b
AynseveN <milkyline129@gmail.com>
2023-10-26 14:17:39 +0800

    update 20231026数据库新增字段

--- uranus/src/main/java/com/ruoyi/domain/UranusPosition.java
+++ uranus/src/main/java/com/ruoyi/domain/UranusPosition.java
@@ -7,9 +7,9 @@ import com.ruoyi.common.core.domain.BaseEntity;
 
 /**
  * 持仓对象 uranus_position
- * 
+ *
  * @author uranus
- * @date 2023-10-25
+ * @date 2023-10-26
  */
 public class UranusPosition extends BaseEntity
 {
@@ -24,42 +24,70 @@ public class UranusPosition extends BaseEntity
 
     /** 持仓净值 */
     @Excel(name = "持仓净值")
-    private String positionNetWorth;
+    private Long positionNetWorth;
 
-    public void setPositionId(Long positionId) 
+    /** 前端展示名称 */
+    @Excel(name = "前端展示名称")
+    private String positionLabel;
+
+    /** 标的占总仓位百分比 */
+    @Excel(name = "标的占总仓位百分比")
+    private Long positionPercent;
+
+    public void setPositionId(Long positionId)
     {
         this.positionId = positionId;
     }
 
-    public Long getPositionId() 
+    public Long getPositionId()
     {
         return positionId;
     }
-    public void setPositionName(String positionName) 
+    public void setPositionName(String positionName)
     {
         this.positionName = positionName;
     }
 
-    public String getPositionName() 
+    public String getPositionName()
     {
         return positionName;
     }
-    public void setPositionNetWorth(String positionNetWorth) 
+    public void setPositionNetWorth(Long positionNetWorth)
     {
         this.positionNetWorth = positionNetWorth;
     }
 
-    public String getPositionNetWorth() 
+    public Long getPositionNetWorth()
     {
         return positionNetWorth;
+    }
+    public void setPositionLabel(String positionLabel)
+    {
+        this.positionLabel = positionLabel;
+    }
+
+    public String getPositionLabel()
+    {
+        return positionLabel;
+    }
+    public void setPositionPercent(Long positionPercent)
+    {
+        this.positionPercent = positionPercent;
+    }
+
+    public Long getPositionPercent()
+    {
+        return positionPercent;
     }
 
     @Override
     public String toString() {
         return new ToStringBuilder(this,ToStringStyle.MULTI_LINE_STYLE)
-            .append("positionId", getPositionId())
-            .append("positionName", getPositionName())
-            .append("positionNetWorth", getPositionNetWorth())
-            .toString();
+                .append("positionId", getPositionId())
+                .append("positionName", getPositionName())
+                .append("positionNetWorth", getPositionNetWorth())
+                .append("positionLabel", getPositionLabel())
+                .append("positionPercent", getPositionPercent())
+                .toString();
     }
 }
